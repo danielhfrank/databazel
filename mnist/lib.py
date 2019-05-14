6955eb8bca1e54cfee165624c4586cbe59003b7c
Dan Frank <danielhfrank@gmail.com>
2019-05-14 15:16:47 -0700

    Checkpoint where we can train and save a model, still busted on loading

--- mnist/lib.py
+++ mnist/lib.py
@@ -1,18 +1,25 @@
 import sys
 
 from keras import backend as K
+from keras.utils import to_categorical
 import numpy as np
 
 # input image dimensions
 img_rows, img_cols = 28, 28
 
-def load_train_data(path):
-    f = np.load(open(path))
-    return f['x_train'], f['y_train']
+# Used by prep_y_data - could infer but whatever
+num_classes = 10
+
 
 def load_train_data(path):
     f = np.load(open(path))
     return f['x_train'], f['y_train']
+
+
+def load_test_data(path):
+    f = np.load(open(path))
+    return f['x_test'], f['y_test']
+
 
 def prep_x_data(x):
     if K.image_data_format() == 'channels_first':
@@ -22,6 +29,11 @@ def prep_x_data(x):
     x = x.astype('float32')
     x /= 255
     return x
+
+
+def prep_y_data(y):
+    return to_categorical(y, num_classes)
+
 
 if __name__ == "__main__":
     data = load_train_data(sys.argv[1])
--- mnist/train.py
+++ mnist/train.py
@@ -1,16 +1,74 @@
 import sys
 
-from mnist.lib import load_train_data, prep_x_data
+import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import keras.backend as K
+import tensorflow as tf
 
-def train(x, y):
-    pass
+from mnist.lib import load_train_data, prep_x_data, prep_y_data
 
-def main(path):
-    x_train, y_train = load_train_data(path)
+batch_size = 128
+# epochs = 12
+epochs = 1
+
+# Should be able to infer...
+num_classes = 10
+
+
+def get_categorical_accuracy_keras(y_true, y_pred):
+    # https://stackoverflow.com/questions/46305252/valueerror-dimension-1-must-be-in-the-range-0-2-in-keras  # noqa
+    return K.mean(K.equal(K.argmax(y_true, axis=1), K.argmax(y_pred, axis=1)))
+
+
+def train(x_train, y_train):
+    input_shape = x_train.shape[1:]
+
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    # model.add(Dense(num_classes, activation='softmax'))
+    model.add(Dense(num_classes, activation=tf.nn.softmax))
+
+
+    model.compile(loss=keras.losses.categorical_crossentropy,
+                  optimizer=keras.optimizers.Adadelta(),
+                  metrics=[get_categorical_accuracy_keras])
+
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1)
+    return model
+
+
+def main(data_path, model_out_path):
+    x_train, y_train = load_train_data(data_path)
     x_train = prep_x_data(x_train)
     # Will need to pass this to keras later on
-    input_shape = x_train.shape[1:]
-    print input_shape
+
+    y_train = prep_y_data(y_train)
+
+    model = train(x_train, y_train)
+
+    # First save model arch only so that I can read it
+    print 'Saving model architecture to json'
+    model_json = model.to_json()
+    with open('model.json', 'w') as f:
+        f.write(model_json)
+
+    print 'Writing entire model (with weights)'
+    model.save(model_out_path)
+
+
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    main(*sys.argv[1:])
